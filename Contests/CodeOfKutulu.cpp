ed6ace9e31e756562d080dfab13f5d309d99a0d5
Mike Schreiber <Mike.Schreiber@VGT.net>
2018-06-15 15:53:41 -0500

    Added storage and printout

--- Contests/CodeOfKutulu.cpp
+++ Contests/CodeOfKutulu.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
 
 using namespace std;
@@ -63,13 +64,7 @@ class Coordinate
 
 class Entity
 {
-	public:
-	int id;
-	Coordinate location;
-	int param0;
-	int param1;
-	int param2;
-
+	protected:
 	Entity(int _id, int _x, int _y, int _param0, int _param1, int _param2)
 	{
 		id = _id;
@@ -79,11 +74,16 @@ class Entity
 		param2 = _param2;
 	}
 	
-	virtual string type() const = 0;
-	virtual Coordinate nextLocation() const = 0;
-};
+	public:
+	int id;
+	Coordinate location;
+	int param0;
+	int param1;
+	int param2;
 
-vector<Entity*> entities;
+	static Entity* Create(string type, int _id, int _x, int _y, int _param0, int _param1, int _param2);
+	virtual string type() const = 0;
+};
 
 class Explorer : public Entity
 {
@@ -100,11 +100,6 @@ class Explorer : public Entity
 	bool isSelf() const
 	{
 		return id == 0;
-	}
-
-	Coordinate nextLocation() const
-	{
-		return location;
 	}
 
 	string type() const
@@ -145,53 +140,181 @@ class Wanderer : public Entity
 		return param2;
 	}
 	
-	void checkNext(Coordinate check, Coordinate target, int &closest, Coordinate &next)
-	{
-		int distance = check.manhattan(target);
-		if (distance < closest)
-		{
-			closest = distance;
-			next = location;
-		}
-	}
-	
-	Coordinate getNextLocation()
-	{
-		if (target() < 0)
-		{
-			return location;
-		}
-		for (auto it = entities.begin(); it != entities.end(); ++it)
-		{
-			if ((*it)->id == target())
-			{
-				int closest = 999999;
-				Coordinate next = location;
-
-				Coordinate check = location;
-				checkNext(check, (*it)->location, closest, next);
-				check.x -= 1;
-				checkNext(check, (*it)->location, closest, next);
-				check.x += 2;
-				checkNext(check, (*it)->location, closest, next);
-				check = location;
-				check.y -= 1;
-				checkNext(check, (*it)->location, closest, next);
-				check.y += 2;
-				checkNext(check, (*it)->location, closest, next);
-				return next;
-			}
-		}
-		
-		return location;
-	}
-	
 	string type() const
 	{
 		return "WANDERER";
 	}
 };
 
+Entity* Entity::Create(string type, int _id, int _x, int _y, int _param0, int _param1, int _param2)
+{
+	if (type.compare("EXPLORER") == 0)
+	{
+		return new Explorer(_id, _x, _y, _param0, _param1, _param2);
+	}
+
+	if (type.compare("WANDERER") == 0)
+	{
+		return new Wanderer(_id, _x, _y, _param0, _param1, _param2);
+	}
+
+	return nullptr;
+}
+
+class Cell
+{
+	public:
+	static Cell* Create(char type);
+	virtual bool inhabitable() const = 0;
+	virtual char print() const = 0;
+};
+
+class Wall : public Cell
+{
+	bool inhabitable() const
+	{
+		return false;
+	}
+	
+	char print() const
+	{
+		return '#';
+	}
+};
+
+class Spawn : public Cell
+{
+	bool inhabitable() const
+	{
+		return true;
+	}
+
+	char print() const
+	{
+		return 'w';
+	}
+};
+
+class Free : public Cell
+{
+	bool inhabitable() const
+	{
+		return true;
+	}
+
+	char print() const
+	{
+		return '.';
+	}
+};
+
+Cell* Cell::Create(char type)
+{
+	switch (type)
+	{
+		case '#':
+			return new Wall();
+		case 'w':
+			return new Spawn();
+		case '.':
+		default:
+			return new Free();
+	}
+}
+
+class Grid
+{
+	public:
+	map<Coordinate, Cell*> cells;
+	vector<Entity*> entities;
+	
+	Grid() {}
+	
+	void addRow(int row, int width, string line)
+	{
+		Coordinate current(0, row);
+		for (auto i = 0; i < width; ++i)
+		{
+			current.x = i;
+			cells[current] = Cell::Create(line[i]);
+		}
+	}
+	
+	void clearEntities()
+	{
+		for (auto it = entities.begin(); it != entities.end(); ++it)
+		{
+			delete (*it);
+		}
+		
+		entities.clear();
+	}
+	
+	void addEntity(string type, int _id, int _x, int _y, int _param0, int _param1, int _param2)
+	{
+		entities.push_back(Entity::Create(type, _id, _x, _y, _param0, _param1, _param2));
+	}
+	
+	friend ostream &operator<<(ostream &os, Grid const &m)
+	{
+		for (auto cell = m.cells.begin(); cell != m.cells.end(); ++cell)
+		{
+			os << cell->second->print();
+			for (auto entity = m.entities.begin(); entity != m.entities.end(); ++entity)
+			{
+				if ((*entity)->location == cell->first)
+				{
+					os << "[" << (*entity)->id << "]";
+				}
+			}
+		}
+		return os;
+	}
+};
+
+Grid grid;
+
+void checkNextLocation(Coordinate check, Coordinate target, int &closest, Coordinate &next)
+{
+	int distance = check.manhattan(target);
+	if (distance < closest)
+	{
+		closest = distance;
+		next = check;
+	}
+}
+
+Coordinate getNextLocation(Wanderer* wanderer)
+{
+	if (wanderer->target() < 0)
+	{
+		return wanderer->location;
+	}
+	for (auto it = grid.entities.begin(); it != grid.entities.end(); ++it)
+	{
+		if ((*it)->id == wanderer->target())
+		{
+			int closest = 999999;
+			Coordinate next = wanderer->location;
+
+			Coordinate check = wanderer->location;
+			checkNextLocation(check, (*it)->location, closest, next);
+			check.x -= 1;
+			checkNextLocation(check, (*it)->location, closest, next);
+			check.x += 2;
+			checkNextLocation(check, (*it)->location, closest, next);
+			check = wanderer->location;
+			check.y -= 1;
+			checkNextLocation(check, (*it)->location, closest, next);
+			check.y += 2;
+			checkNextLocation(check, (*it)->location, closest, next);
+			return next;
+		}
+	}
+	
+	return wanderer->location;
+}
+	
 /**
  * Survive the wrath of Kutulu
  * Coded fearlessly by JohnnyYuge & nmahoude (ok we might have been a bit scared by the old god...but don't say anything)
@@ -205,6 +328,7 @@ int main()
     for (int i = 0; i < height; i++) {
         string line;
         getline(cin, line);
+		grid.addRow(i, width, line);
     }
     int sanityLossLonely; // how much sanity you lose every turn when alone, always 3 until wood 1
     int sanityLossGroup; // how much sanity you lose every turn when near another player, always 1 until wood 1
@@ -216,6 +340,7 @@ int main()
     while (1) {
         int entityCount; // the first given entity corresponds to your explorer
         cin >> entityCount; cin.ignore();
+		grid.clearEntities();
         for (int i = 0; i < entityCount; i++) {
             string entityType;
             int id;
@@ -225,8 +350,10 @@ int main()
             int param1;
             int param2;
             cin >> entityType >> id >> x >> y >> param0 >> param1 >> param2; cin.ignore();
+			grid.addEntity(entityType, id, x, y, param0, param1, param2);
         }
 
+		cerr << grid << endl;
         // Write an action using cout. DON'T FORGET THE "<< endl"
         // To debug: cerr << "Debug messages..." << endl;
 
