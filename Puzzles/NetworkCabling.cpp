294a0898fd6094bd2b51364fd3900ec12029cbf1
MooseHole <moose_hole@yahoo.com>
2023-02-27 14:06:51 -0700

    Removed debug messages.  Works!

--- Puzzles/NetworkCabling.cpp
+++ Puzzles/NetworkCabling.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 #include <climits>
 #include <map>
 
@@ -9,50 +7,28 @@ using namespace std;
 int main()
 {
     int N;
-    int minY;
-    int maxY;
-    int minX;
-    int maxX;
+    int minX = INT_MAX;
+    int maxX = INT_MIN;
     ulong length;
 
     map<int, int> houseYUnique;
     cin >> N; cin.ignore();
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)
+    {
         int X;
         int Y;
         cin >> X >> Y; cin.ignore();
-        cerr << X << " " << Y << endl;
         
         houseYUnique[Y] += 1;
-        
-        if (i == 0)
+
+        if (X < minX)
         {
             minX = X;
-            maxX = X;
-            minY = Y;
-            maxY = Y;
         }
-        else
+            
+        if (X > maxX)
         {
-            if (Y < minY)
-            {
-                minY = Y;
-            }
-            
-            if (Y > maxY)
-            {
-                maxY = Y;
-            }
-
-            if (X < minX)
-            {
-                minX = X;
-            }
-            
-            if (X > maxX)
-            {
-                maxX = X;
-            }
+            maxX = X;
         }
     }
 
@@ -79,13 +55,11 @@ int main()
 
     length = trunkLength;
 
-    cerr << "Trunk at Y=" << trunkY << " Trunk length " << trunkLength << endl;
 
     for (map<int, int>::iterator nodeY = houseYUnique.begin(); nodeY != houseYUnique.end(); ++nodeY)
     {
         ulong dedicated = abs(trunkY - (*nodeY).first) * (*nodeY).second;
         length += dedicated;
-        cerr << "add " << dedicated << " for house at Y=" << (*nodeY).first << " total " << length << endl;
     }
         
     // Write an action using cout. DON'T FORGET THE "<< endl"
